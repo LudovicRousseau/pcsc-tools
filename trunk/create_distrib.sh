d4dc0c252c32073c6758429222de9fadd32d7416
Ludovic Rousseau <ludovic.rousseau@free.fr>
2001-10-17 09:22:11 +0000

    Added checks: directory name format, directory existance
    git-svn-id: svn://anonscm.debian.org/svn/pcsclite/trunk/pcsc-tools@3534 0ce88b0d-b2fd-0310-8134-9614164e65ea

--- trunk/create_distrib.sh
+++ trunk/create_distrib.sh
@@ -1,15 +1,35 @@
 #/bin/sh -e
 
-# $Id: create_distrib.sh,v 1.1 2001-10-16 07:33:22 rousseau Exp $
+# $Id: create_distrib.sh,v 1.2 2001-10-17 09:22:11 rousseau Exp $
 
 # $Log: not supported by cvs2svn $
+# Revision 1.1  2001/10/16 07:33:22  rousseau
+# script to export a clean archive
+#
+
+# create a new directory named after the current directory name
+# the directory name should be in the form foo-bar.x.y.z
+# the use of "_" is not recommanded since it is a problem for Debian
 
 dir=$(basename $(pwd))
-if [ -d $dir ]
+
+echo -e "Using $dir as directory name\n"
+
+rv=$(echo $dir | sed -e 's/.*-[0-9]\.[0-9]\.[0-9]/ok/')
+if [ $rv != "ok" ]
 then
-	rm -r $dir
+	echo "ERROR: The directory name should be in the form foo-bar.x.y.z"
+	exit
+fi
+
+if [ -e $dir ]
+then
+	echo -e "ERROR: $dir already exists\nremove it and restart"
+	exit
 fi
 mkdir $dir
+
+rcs2log > $dir/Changelog
 
 for i in $(cat MANIFEST)
 do
@@ -25,8 +45,6 @@ do
 	echo "cp $i $dir/$i"
 	cp -a $i $dir/$i
 done
-
-rcs2log > $dir/Changelog
 
 tar czvf ../$dir.tar.gz $dir
 rm -r $dir
