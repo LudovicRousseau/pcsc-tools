77ec286157112dc1a523701089747cbf3d5b9d83
Ludovic Rousseau <ludovic.rousseau@free.fr>
2019-09-08 18:11:30 +0200

    sync: update smartcard_list.txt on https://smartcard-atr.apdu.fr/

--- sync.sh
+++ sync.sh
@@ -2,3 +2,5 @@ set -x
 cp smartcard_list.txt ~/public_html.free/softwares/pcsc-tools/smartcard_list.txt
 #cp smartcard_list.txt ~/.cache/smartcard_list.txt
 sitecopy -u ftpperso
+
+scp ~/Documents/sc/costa/pcsc-tools/smartcard_list.txt vps:Serveurs_web/smartcard-atr.apdu.fr
