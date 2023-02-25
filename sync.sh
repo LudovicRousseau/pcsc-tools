327af78fee1fbee816b13d003370820d6bfcfb8e
Ludovic Rousseau <ludovic.rousseau@free.fr>
2023-02-25 23:24:31 +0100

    sync.sh: remove update of http://ludovic.rousseau.free.fr/softwares/
    We now have a redirection using .htaccess
    Redirect permanent /softwares/pcsc-tools/smartcard_list.txt https://pcsc-tools.apdu.fr/smartcard_list.txt

--- sync.sh
+++ sync.sh
@@ -5,8 +5,4 @@ set -x
 
 ./sort_smartcard_list.py
 
-cp smartcard_list.txt ~/public_html.free/softwares/pcsc-tools/smartcard_list.txt
-#cp smartcard_list.txt ~/.cache/smartcard_list.txt
-sitecopy -u ftpperso
-
 scp ~/Documents/sc/costa/pcsc-tools/smartcard_list.txt vps:Serveurs_web/pcsc-tools.apdu.fr
